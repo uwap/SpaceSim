dedb38788cf71d66a0b6cd26eb6b28955296ea00
uwap <me@uwap.name>
2015-11-28 04:09:41 +0100

    Implement a basic entity scheme

---
+++ src/Entity.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Entity where
+
+import Graphics.Rendering.OpenGL (TextureObject)
+import Control.Lens
+import Linear
+
+data Entity = Entity { _coord   :: !(V2 Double)
+                     , _size    :: !(V2 Double)
+                     , _zlevel  :: !Int
+                     , _texture :: !(Maybe TextureObject)
+                     , _update  :: !(Entity -> [Entity])
+                     }
+makeLenses ''Entity
+
+tile :: V2 Double -> TextureObject -> Entity
+tile coord tex = Entity coord (V2 64 64) 0 (Just tex) pure
