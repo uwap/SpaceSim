fe2d2665dc8891c0c644d6315cb524c4e149d471
uwap <me@uwap.name>
2015-11-30 04:48:07 +0100

    Make Entity use Texture Type and implement updateAll function

--- src/Entity.hs
+++ src/Entity.hs
@@ -1,18 +1,27 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Entity where
 
 import Game
-import Graphics.Rendering.OpenGL (TextureObject)
+import Resources
 import Control.Lens
+import Control.Monad
+import Control.Monad.State.Strict
 import Linear
 
 data Entity = Entity { _coord   :: !(V2 Double)
                      , _size    :: !(V2 Double)
                      , _zlevel  :: !Int
-                     , _texture :: !(Maybe TextureObject)
+                     , _texture :: !(Maybe Texture)
                      , _update  :: !(Entity -> Game Entity)
                      }
 makeLenses ''Entity
 
-tile :: V2 Double -> TextureObject -> Entity
+tile :: V2 Double -> Texture -> Entity
 tile coord tex = Entity coord (V2 64 64) 0 (Just tex) pure
+
+updateAll :: (MonadState s m, Traversable t) => Lens' s (t e) -> Lens' e (e -> m e) -> m ()
+updateAll entities update = do
+  all <- use entities
+  entities <~ traverse (\x -> x & x ^. update) all
